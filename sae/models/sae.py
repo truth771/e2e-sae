8d2e07d72837aef1cf29484104485b5a781e70b6
Ruthesh-Thavamani <truthesh@gmail.com>
2025-12-05 18:28:36 -0500

    updated sae

--- sae/models/sae.py
+++ sae/models/sae.py
@@ -1,6 +1,7 @@
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
+import losses
 
 class SAE_Local(nn.Module):
     """Sparse Autoencoder to reconstruct activations of a particular layer."""
@@ -11,12 +12,18 @@ class SAE_Local(nn.Module):
 
         self.phi = sparsity_param / d_dict
 
-        # might need dictionary normalization here?
+    # normalize every time after update
+    def normalize_dictionary(self):
+        with torch.no_grad():
+            W = self.encoder.weight.data  # shape: (d_dict, d_model)
+            W_norm = W.norm(p=2, dim=1, keepdim=True)  # shape: (d_dict, 1)
+            W = W / W_norm
+            self.encoder.weight.data = W
 
     def forward(self, x):
         z = F.relu(self.encoder(x))
         x_reconstructed = self.decoder(z)
-        return x_reconstructed, z
+        return x_reconstructed, losses.l1_penalty(z, self.phi * x.size(-1), x.size(-1))
     
     def loss(self, x, z, x_reconstructed):
         return F.mse_loss(x_reconstructed, x) + self.phi * torch.mean(z)
