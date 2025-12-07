b5a8ece406905a2085e3d08f5279451c4a595b5c
vivaansinghvi07 <singhvi.vivaan@gmail.com>
2025-12-06 21:49:05 -0500

    added more stuff to the training script

--- sae/training/train.py
+++ sae/training/train.py
@@ -1,5 +1,6 @@
-from typing import Literal
+from typing import Literal, Container
 
+import tqdm 
 import torch
 import torch.nn.functional as F
 
@@ -7,8 +8,11 @@ from ..models import get_model, SAEParams
 from .datasets import get_openwebtext_dataloaders, OpenWebTextConfig
 
 
-def train(model_str: Literal["gpt2", "llama"], sae_params: SAEParams):
-    train_loader, _ = get_openwebtext_dataloaders(OpenWebTextConfig(), 1)
+def train(model_str: Literal["gpt2", "llama"], sae_params: SAEParams, 
+          batch_size: int = 32, n_epochs: int = 20, checkpoint_epochs: Container = {},
+          sparsity_weight: float = 4.0, mse_weight: float = 2.5):
+
+    train_loader, _ = get_openwebtext_dataloaders(OpenWebTextConfig(), batch_size)
     normal_model, _ = get_model(model_str, SAEParams())
     model, trainable_params = get_model(model_str, sae_params)
     
@@ -17,30 +21,33 @@ def train(model_str: Literal["gpt2", "llama"], sae_params: SAEParams):
 
     optimizer = torch.optim.Adam(trainable_params, lr=0.005)
 
-    for batch in train_loader:
-        optimizer.zero_grad()
-        logits, *_, (mse_losses, _, sparsity_param) = model(batch["input_ids"])
-        normal_logits, *_ = normal_model(batch["input_ids"])
+    for epoch in range(n_epochs):
+        for batch in tqdm.tqdm(train_loader, desc=f"epoch {epoch + 1}"):
+            optimizer.zero_grad()
+            logits, *_, (mse_losses, _, sparsity_param) = model(batch["input_ids"])
+            normal_logits, *_ = normal_model(batch["input_ids"])
 
-        kl_loss = F.kl_div(
-            F.log_softmax(logits, dim=-1),
-            F.log_softmax(normal_logits, dim=-1),
-            log_target=True,
-            reduction="batchmean",
-        )
+            kl_loss = F.kl_div(
+                F.log_softmax(logits, dim=-1),
+                F.log_softmax(normal_logits, dim=-1),
+                log_target=True,
+                reduction="batchmean",
+            )
 
-        match sae_params.sae_type:
-            case "e2e":
-                total_loss = kl_loss + sparsity_weight * sparsity_param
-            case "e2e + ds":
-                total_loss = kl_loss + sparsity_weight * sparsity_param + mse_weight * mse_losses
-            case "local":
-                total_loss = mse_losses
-            case _:
-                raise ValueError("Unexpected sae_type encountered.")
+            match sae_params.sae_type:
+                case "e2e":
+                    total_loss = kl_loss + sparsity_weight * sparsity_param
+                case "e2e + ds":
+                    total_loss = kl_loss + sparsity_weight * sparsity_param + mse_weight * mse_losses
+                case "local":
+                    total_loss = mse_losses
+                case _:
+                    raise ValueError("Unexpected sae_type encountered.")
 
-        total_loss.backward()
-        optimizer.step()
+            total_loss.backward()
+            optimizer.step()
+        if epoch in checkpoint_epochs:
+            torch.save(model.state_dict(), f"model-{sae_params.sae_layer}-{sae_params.sae_dict_size}-{sae_params.sae_type}-epoch-{epoch}.pth")
 
 if __name__ == "__main__":
     train("gpt2", SAEParams(6, 1000, "local"))
